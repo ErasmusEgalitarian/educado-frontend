0672d301df26aeddeba078b2868a899ea1458746
pernilje <94057672+pernilje@users.noreply.github.com>
2023-10-05 13:44:43 +0200

    styling in create course
    colors added
    -       primary: '#5ECCE9',
            secondary: '#F1F9FB',
            tertiary: '#FFFFFF',
            primaryHover: ' #4DAACB',
            primaryLight: '#c9e4eb',
            warning: '#FF4949'

--- app/src/components/Courses/CreateCourseModal.tsx
+++ app/src/components/Courses/CreateCourseModal.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useSWRConfig } from 'swr';
+import {Dropzone} from '../Dropzone/Dropzone';   
+
+
 
 // Contexts
 // import useAuthStore from '../../contexts/useAuthStore';
@@ -29,7 +32,7 @@ type Inputs = {
     title: string,
     category: string,
     level: string,
-    coverImageId: number,
+    time: number,
     description: string,
 };
 
@@ -56,7 +59,7 @@ export const CreateCourseModal = () => {
             title: data.title,
             category: data.category,
             level: data.level,
-            coverImageId: data.coverImageId,
+            time: data.time,
             description: data.description,
         }, token)
             .then(res => console.log(res))
@@ -74,28 +77,28 @@ export const CreateCourseModal = () => {
             {/* Put this part before </body> tag */}
             <input type="checkbox" id="course-create" className="modal-toggle" />
             {
-                onclick = function () {StorageServices.uploadFile({bucketName: "educado-bucket", id: "gorilla", filePath: "/images/gorilla.jpg"});}
+                onclick = function () {StorageServices.uploadFile({bucketName: "educado-bucket", id: "testFoto", filePath: "c:/Users/perni/Downloads/settings_icon.png"});}
             }
             <div className="modal" id="course-create-modal">
-                <div className="modal-box rounded w-11/12 max-w-xl">
+                <div className="modal-box bg-gradient-to-b from-primaryLight rounded w-11/12 max-w-xl">
                     <h3 className="font-bold text-lg">Crie seu novo curso!</h3>
                     <p className="py-4">Preencha o formulário e comece seu novo curso!</p>
-                    <img src='https://storage.googleapis.com/educado-bucket/gorilla'></img>
+                   
 
                     <form className="flex h-full flex-col justify-between space-y-4" onSubmit={handleSubmit(onSubmit)}>
                         <div className="flex flex-col space-y-2 text-left">
                             <label htmlFor='title'>Título</label>
                             <input type="text" defaultValue={""}
-                                className="form-field focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                                className="form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
                                 {...register("title", { required: true })}
                             />
-                            {errors.title && <span>Este campo é obrigatório</span>}
+                            {errors.title && <span className='text-warning'>Este campo é obrigatório</span>}
                         </div>
 
                         <div className="flex flex-col space-y-2 text-left">
                             <label htmlFor='category'>Categoria</label>
                             <select defaultValue={"Escolher categoria"} /*Choose category*/
-                                className="form-field focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                                className="form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
                                 {...register("category", { required: true })}
                             >
                                 {/*hard coded options by PO, should be changed to get from db*/}
@@ -104,14 +107,22 @@ export const CreateCourseModal = () => {
                                 <option>Costura </option> {/*Sewing*/}
                                 <option>Eletrônica </option> {/*Electronics*/}
                             </select>
-                            {errors.description && <span>Este campo é obrigatório</span>}
+                            {errors.description && <span className='text-warning'>Este campo é obrigatório</span>}
                         </div>
 
-                        <div className="flex items-center gap-10 w-full mt-8">
+                        <div className="flex items-center gap-8 w-full mt-8">
+                            
+                            {/*cover image feild is made but does not interact with the db*/}
+                            <div className="flex flex-col space-y-2 text-left">    
+                            <label htmlFor='cover-image'>Imagem de capa</label>
+                                    <Dropzone></Dropzone>
+                                {errors.description && <span className='text-warning'>Este campo é obrigatório</span>}
+                            </div>
+
                             <div  className="flex flex-col space-y-2 text-left">
                                 <label htmlFor='level'>Nível</label>
                                 <select defaultValue={"Escolher categoria"} /*Choose category*/
-                                    className="small-form-field focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                                    className="small-form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
                                     {...register("level", { required: true })}
                                 >
                                     {/*hard coded options by PO, should be changed to get from db*/}
@@ -120,53 +131,38 @@ export const CreateCourseModal = () => {
                                     <option>Avançado </option> {/*...*/}
                                 
                                 </select>
-                                {errors.description && <span>Este campo é obrigatório</span>}
+                                {errors.description && <span className='text-warning'>Este campo é obrigatório</span>}
                             </div>
 
                             <div  className="flex flex-col space-y-2 text-left">
                                 <label htmlFor='title'>Tempo estimado</label>
                                 <input type="number" defaultValue={""} min={0} step={1}
-                                    className="extra-small-form-field focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-                                    {...register("title", { required: true })}
+                                    className="extra-small-form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                                    {...register("time", { required: true })}
                                 />
-                                {errors.title && <span>Este campo é obrigatório</span>}
+                                {errors.title && <span className='text-warning'>Este campo é obrigatório</span>}
                             </div>
                         
-                            {/*cover image feild is made but does not interact with the db*/}
-                            <div className="flex flex-col space-y-2 text-left">
-                                
-                                <label htmlFor='cover-image'>Imagem de capa</label>
-                                <input type="file" defaultValue={""}
-                                    className="extra-small-form-field focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-                                    {
-                                        ...register("coverImageId", { required: true, setValueAs: (value) => {
-
-                                           
-                                        }})
-                                
-                                    }
-                                />
-                                {errors.description && <span>Este campo é obrigatório</span>}
-                            </div>
+                       
                         </div>
 
                         <div className="flex flex-col space-y-2 text-left">
                             <label htmlFor='description'>Descrição</label>
                             <textarea rows={4} defaultValue={""}
-                                className="resize-none form-field focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                                className="resize-none form-field focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
                                 {...register("description", { required: true })}
                             />
-                            {errors.description && <span>Este campo é obrigatório</span>}
+                            {errors.description && <span className='text-warning'>Este campo é obrigatório</span>}
                         </div>
 
                         <div className='modal-action'>
                             <div className="flex items-center justify-between gap-4 w-full mt-8">
-                                <label htmlFor='course-create' className="py-2 px-4  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded">
+                                <label htmlFor='course-create' className="py-2 px-4  bg-primary hover:bg-primaryHover focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded">
                                     <button type="submit">
                                         Criar
                                     </button>
                                 </label>
-                                <label htmlFor='course-create' className="py-2 px-4 bg-white hover:bg-gray-100 border border-blue-500 focus:ring-blue-500 focus:ring-offset-blue-200 text-blue-500 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded">
+                                <label htmlFor='course-create' className="py-2 px-4 bg-white hover:bg-gray-100 border border-primary  hover:border-primaryHover hover:text-primaryHover  text-primary w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded">
                                     Cancelar
                                 </label>
                             </div>
--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -5,13 +5,24 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {}
+ 
+    extend: {
+      colors: {
+        primary: '#5ECCE9',
+        secondary: '#F1F9FB',
+        tertiary: '#FFFFFF',
+        primaryHover: ' #4DAACB',
+        primaryLight: '#c9e4eb',
+        warning: '#FF4949'
+      },
+    },
+    
   },
   plugins: [
     require('@tailwindcss/forms'),
     require("daisyui")
   ],
   daisyui: {
-    themes: false,
+    themes: true,
   },
 }
