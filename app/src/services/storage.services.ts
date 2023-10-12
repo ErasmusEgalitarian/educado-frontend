e402e35740a44f8b77def10604cfb6eac71ce0be
Slovenly <54136932+SlovenlyTF@users.noreply.github.com>
2023-10-12 10:37:31 +0200

    small fix after linting

--- app/src/services/storage.services.ts
+++ app/src/services/storage.services.ts
@@ -22,7 +22,7 @@ interface FileProps {
  * @returns {void}
  */
 async function uploadFile ({ bucketName, filePath, id }: FileProps) {
-  axios.put(`https://storage.googleapis.com/${bucketName}/${id}`, {
+  return await axios.put(`https://storage.googleapis.com/${bucketName}/${id}`, {
     body: filePath,
     headers: {
       // authorization: `Bearer ${"-----BEGIN CERTIFICATE-----\nMIIC/DCCAeSgAwIBAgIIc58ZbfOHMD0wDQYJKoZIhvcNAQEFBQAwIDEeMBwGA1UE\nAxMVMTE0MzUyMzQwMDMxOTQ0OTY0Mjg2MCAXDTIzMDkyMDA5NDI0MloYDzk5OTkx\nMjMxMjM1OTU5WjAgMR4wHAYDVQQDExUxMTQzNTIzNDAwMzE5NDQ5NjQyODYwggEi\nMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDWXm/KOvOp9WcT6HX1cQ8mnkv3\nZgrlHwqibTdri4Ge+w5rzlm6thGqvuols40ND28D8iFj1wyLyEmBNkBNPlOmuR+3\ngcuHTjfqV6Dl4pUp9UXSBm76YsZ0wHSyGFWbd8ujXljTEdVSFxI+eg2u4sRj8Ukf\nym0xtiITfXy/7D2luarUrQ6/zD5RG8oTspObg5NyRWVutqVEqqq5R8nNwhtI3KQf\ny8brm80VxvfuYz6Epv5lhN3lQFaaUaecHQhb6R+B0UKHEjtmhc+DxzoSFHoG6rNu\n4kZF0SolM3GfspwAzBgCvdGXzZsxtlX2YPzG+zNWLx9vyZ3cwfSEkPrvdUX/AgMB\nAAGjODA2MAwGA1UdEwEB/wQCMAAwDgYDVR0PAQH/BAQDAgeAMBYGA1UdJQEB/wQM\nMAoGCCsGAQUFBwMCMA0GCSqGSIb3DQEBBQUAA4IBAQAnbmkssgdtNHsi7Uu+Ks4R\nb9CllqEJwbEAEQWHfMjBRo9uh6F8WLy91aKoRGPZgqhh9++r9kHZYKcheAKE9z8d\no7MFH6tqjikAisu6rjowxpyv8TfJ7T71hOQBKGGNAfg2bWtMkGmac/QvrbtaPV47\nw3V0Xjm7wKLKEqiEmPhdrPE7EOStlKyfi/q8xK73tCLKnwo4O5SsRTIpiZYnUdYI\nU6hwzeW7+zUIfgketc/aZlspXBw+rvZ2R5GR000++mSbZW4ylNxbfp8d64kmwstR\nKXqLcEyqO+0n3secK5O0yoVFPJxFqgX8eYJTSPaQAqcuSzV+LvRGy5Tcp0fQC8HP\n-----END CERTIFICATE-----\n"}`,
--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,1 +1,1 @@
-import <reference types="vite/client" />
+///<reference types="vite/client" />
