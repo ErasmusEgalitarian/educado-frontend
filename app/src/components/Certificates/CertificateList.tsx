801d3c11b0a3dd10bf48ef936a15e2a53dcfb73b
Jatewo <jacobterpewoods@gmail.com>
2023-11-21 15:18:37 +0100

    feat: Created quick empty page for certificates

--- app/src/components/Certificates/CertificateList.tsx
+++ app/src/components/Certificates/CertificateList.tsx
@@ -4,6 +4,7 @@ import { Certificate } from "../../interfaces/Certificate";
 import CertificateCard from "./CertificateCard";
 import { useEffect, useState } from "react";
 import CertificateService from "../../services/certificate.services";
+import EmptyImg from "../../assets/no-courses.png";
 
 export default function CertificateList() {
 	const token = getUserToken();
@@ -28,10 +29,20 @@ export default function CertificateList() {
 						<p className="text-grayMedium">You have {certificates.length} certificates</p>
 					</div>
 					{certificates.map((certificate: Certificate, key: number) => (
-							<CertificateCard certificate={certificate} key={key} num={key} />
+						<CertificateCard certificate={certificate} key={key} num={key} />
 					))}
 				</> :
-				<div id="no-certificates-message" className="text-xl">Empty</div>
+				// If the user has no courses, display this 'empty state'
+				<div className='grid place-content-center w-full h-full text-center'>
+				<div className='md:mx-40 xl:mx-64' id="no-certificates-message">
+					<img src={EmptyImg} className='w-full' />
+					<h1 className='te	xt-xl font-bold my-4'>Comece agora</h1>
+					{/* It appears you haven't created any courses yet.
+					Click the button below to see your course page. */}
+					<p>Parece que você ainda não criou nenhum curso. Clique no botão abaixo para acessar sua página de cursos.</p>
+				</div>
+			</div>
+
 			}
 		</div>
 	)
