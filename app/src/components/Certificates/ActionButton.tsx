2a084d37c6f8c95d93140fa57d4eb0d48e915b8c
Jatewo <jacobterpewoods@gmail.com>
2023-11-24 14:52:41 +0100

    feat: download and preview of certificate PDFs
    Changes made:
    - Added onClick to ActionButton for download and preview functions
    - Added on click functions and a preview object element to certificate cards
    - Added _id to contentCreator interface
    - Fixed overflow behaviour for certificate list

--- app/src/components/Certificates/ActionButton.tsx
+++ app/src/components/Certificates/ActionButton.tsx
@@ -1,4 +1,5 @@
 import Icon from "@mdi/react";
+import { useState } from "react";
 
 type propTypes = {
 	children: React.ReactNode;
@@ -8,8 +9,9 @@ type propTypes = {
 }
 
 export default function ActionButton(props: propTypes) {
+
 	return (
-		<button className="std-button rounded-lg px-7 cursor-pointer">
+		<button className="std-button rounded-lg px-7 cursor-pointer" onClick={props.onClick}>
 			{props.children}
 			{props.icon ? <Icon path={props.icon} className='w-6 h-6 text-white float-right' /> : null}
 		</button>
