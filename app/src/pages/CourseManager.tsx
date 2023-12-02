fc04322f9cedb7534f37991fae9e4bb4b9e1d421
sofiiagran <sofiiagran@gmail.com>
2023-12-02 12:58:00 +0100

    Update CourseManager.tsx

--- app/src/pages/CourseManager.tsx
+++ app/src/pages/CourseManager.tsx
@@ -1,46 +1,47 @@
-import Checklist from "../components/Checklist"
-import Layout from "../components/Layout"
-import { CourseComponent } from "../components/Courses/CourseComponent"
-import { SectionCreation } from "../components/SectionCreation"
-import { useState } from "react"
-import { useParams } from "react-router"
-
+import Checklist from "../components/Checklist";
+import Layout from "../components/Layout";
+import { CourseComponent } from "../components/Courses/CourseComponent";
+import { SectionCreation } from "../components/SectionCreation";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { getUserToken } from '../helpers/userInfo';
 
-
 const CourseManager = () => {
-  
   const token = getUserToken();
-  let { id, tick } = useParams();
-  
-  
-  const [tickChange, setTickChange] = useState<number>(parseInt(tick ?? "0"))
-  const [formComponents, setFormComponents] = useState<JSX.Element[]>
-  ([
-    <CourseComponent token={token} id={id} setTickChange={setTickChange} setId={setId}/>,
-    <SectionCreation id={id ?? ""} token={token} setTickChange={setTickChange}/>
-  ])
+  const { id: paramId, tick: paramTick } = useParams();
 
-  function setId(idInput: string){
-    id = idInput;
+  // Ensure id and tick are valid integers or default to 0
+  const id = parseInt(paramId ?? "0");
+  const tick = parseInt(paramTick ?? "0");
+
+  const [tickChange, setTickChange] = useState<number>(tick);
+  const [formComponents, setFormComponents] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+    // Update formComponents based on tickChange
+    const components: JSX.Element[] = [
+      <CourseComponent token={token} id={id} setTickChange={setTickChange} setId={setId} />,
+      <SectionCreation id={id.toString()} token={token} setTickChange={setTickChange} />
+    ];
+    setFormComponents(components);
+  }, [id, token]);
+
+  function setId(idInput: string) {
+    // Implement setId logic if needed
   }
 
   return (
     <Layout meta="Course Manager">
       <div className="flex flex-row">
+        <Checklist tickChange={tickChange} />
 
-          <Checklist tickChange={tickChange}/>
-        
-          {/* Component renderer */}
-          <div className='flex-none w-2/3  mr-20'>
-              {/*{tickChange === 0 ? <CourseCreationCom token={token} id={id}/> :  <p>An error has occured</p>} */}
-              {formComponents[tickChange]}
-          </div>
+        {/* Component renderer */}
+        <div className='flex-none w-2/3  mr-20'>
+          {formComponents[tickChange] || <p>Component not found</p>}
+        </div>
       </div>
-
     </Layout>
-  )
-}
+  );
+};
 
-
-export default CourseManager
+export default CourseManager;
