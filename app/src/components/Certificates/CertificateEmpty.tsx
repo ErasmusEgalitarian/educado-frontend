e8f1828544ba28a6a8051d37200785a4ab2c13ef
Frederik Woloszynski <frederikwoloszynski@hotmail.com>
2024-10-07 13:42:25 +0200

    Update on design prefences from Iara

--- app/src/components/Certificates/CertificateEmpty.tsx
+++ app/src/components/Certificates/CertificateEmpty.tsx
@@ -13,7 +13,7 @@ const emptyState = () => {
         <div className="flex flex-col items-center justify-center w-full h-full">
             <div className='flex flex-col items-center text-center justify-center' id="no-certificates-message">
                 <img src={noCertificatesImage} className='w-1/3 h-auto mx-auto' />
-                <div className='w-[75%]'>
+                <div className='w-[70%]'>
                     <h1 className='text-xl flex-1 font-bold my-4'>Você ainda não tem nenhum certificado.</h1>
                     <p className='text-darkGray flex-1 '>Clique no botão abaixo e siga o passo a passo para desenolver o seu primeiro curso.</p>
                     <label htmlFor="course-create" onClick={CourseManager} className="std-button justify-center text-center items-center flex-1 flex modal-button  space-x-">
