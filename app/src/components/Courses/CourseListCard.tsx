4214758ffccb2ad6acbedef80ffcd2a3491e9afa
davidfraenkel <david.fraenkel@hotmail.com>
2024-11-25 13:58:00 +0100

    New design on courseCard, Sorting and searching in courses

--- app/src/components/Courses/CourseListCard.tsx
+++ app/src/components/Courses/CourseListCard.tsx
@@ -6,18 +6,11 @@ import StarRating from "../general/StarRating";
 import { Icon } from '@mdi/react';
 import { mdiPencil, mdiAccount } from '@mdi/js';
 import { LastEdited } from "./LastEdited";
+import { BsClockFill } from "react-icons/bs";
 
 // Helpers
 import categories from "../../helpers/courseCategories";
 import statuses from "../../helpers/courseStatuses";
-
-// Images
-import imageNotFoundImage from '../../assets/image-not-found.png';
-import { useEffect, useState } from "react";
-import StorageServices from "../../services/storage.services";
-
-// Rating
-import { getAverageRatingOfCC, getAverageRatingOfCourse } from '../../services/contentCreator.services';
 
 /**
  * Displays a course in a card format
@@ -26,109 +19,55 @@ import { getAverageRatingOfCC, getAverageRatingOfCourse } from '../../services/c
  * @returns HTML Element
  */
 export const CourseListCard = ({ course }: { course: Course }) => {
-  const [imageSrc, setImageSrc] = useState<string>('');
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   const maxTitleLength = 20;
   //Only load the picture, when the picture is loaded
-  useEffect(() => {
-    const fetchImage = async () => {
-      try {
-        if(course.coverImg == "" || course.coverImg == undefined) {
-          throw new Error("coverImg is empty or undefined");
-        }
-        const fileSrc = await StorageServices.getMedia(course.coverImg);
-
-        setImageSrc(fileSrc);
-
-      } catch (error) {
-        setImageSrc(imageNotFoundImage);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    const fetchRating = async () => {
-      try {
-        const rating = await getAverageRatingOfCourse(course._id);
-        console.log("card" +rating);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    fetchRating();
-    fetchImage();
-  }, [course.coverImg, course.title]);
+ 
   return (
-    <div className="overflow-hidden shadow rounded h-full w-full cursor-pointer m-auto hover:shadow-lg duration-200">
+    <div className="shadow-courseCard border rounded-lg h-full w-full cursor-pointer m-auto ">
       <label
         onClick={()=>window.location.href = `/courses/manager/${course._id}/0`}
         className="w-full block h-full relative group"
       >
-
-        {/* Hover overlay */}
-        <div className='absolute inset-0 grid place-content-center'>
-          {/* Edit button */}
-          <div className='z-10 bg-primary opacity-0 rounded-full p-8 group-hover:animate-slidePopIn duration-200 group-hover:opacity-100'>
-            <Icon path={mdiPencil} className='w-12 text-white' />
-          </div>
-          <div className='z-0 absolute inset-0 bg-white opacity-0 group-hover:opacity-30 duration-200' />
-        </div>
-
-        {/* Card image */}
-        {isLoading ? (
-          <div className="h-40 w-full bg-gray-200 animate-pulse" />
-        ) : (
-          <img
-            src={imageSrc}
-            className="h-40 w-full object-cover bg-white border-b"
-          />
-        )}
-
         {/* Card content */}
         <div className="bg-white w-full">
           <div className='p-4'>
             {/* Card title */}
-            <h2 className="text-gray-800 text-xl font-medium mb-2">
-              {course.title.slice(0, maxTitleLength) + (course.title.length > maxTitleLength ? '...' : '')}
-            </h2>
+            <div className="flex items-center gap-5 border-b border-lightgray pb-2 my-5">
 
-            <div className="flex flex-row justify-between">
+            <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20 18H0V0H2V16H4V14H8V16H10V13H14V16H16V14H20V18ZM16 11H20V13H16V11ZM10 3H14V6H10V3ZM14 12H10V7H14V12ZM4 7H8V9H4V7ZM8 13H4V10H8V13Z" fill="#383838"/>
+            </svg>
+
+              <h2 className="text-gray-800 text-xl font-medium">
+                {course.title.slice(0, maxTitleLength) + (course.title.length > maxTitleLength ? '...' : '')}
+              </h2>
+            </div>
+
+
+            {/* Card info */}
+            <div className='flex flex-col justify-between mt-8 text-grayMedium'>
+              {/* Course category */}
+              <div className='flex flex-row gap-5'>
+                <div className="flex flex-row">
+                  <Icon
+                    path={categories[course.category]?.icon ?? categories.default.icon}
+                    className='w-4 mr-1'
+                    />
+                  <p>{categories[course.category]?.br ?? course.category}</p>
+                </div>
+                <div className="flex flex-row items-center gap-1">
+                  <BsClockFill />
+                  <p>{(course.estimatedHours != null) ? course.estimatedHours : "?"} horas</p>
+                </div>
+              </div>
+            
               {/* Course rating */}
               <div className='w-[8rem]'>
                 <StarRating rating={course.rating ?? 0} />
               </div>
-              {/* Subsriber count */}
-              <div className='flex flex-row'>
-                <Icon path={mdiAccount} className='w-5 mr-1 inline-block mr-1' />
-                <p className='text-grayMedium'>{course.numOfSubscriptions}</p>
-              </div>
-            </div>
+              
 
-            {/* Card info */}
-            <div className='h-20 flex flex-col justify-between'>
-              {/* Course category */}
-              <div className='flex flex-row'>
-                <Icon
-                  path={categories[course.category]?.icon ?? categories.default.icon}
-                  className='w-4 mr-1'
-                />
-                <p>{categories[course.category]?.br ?? course.category}</p>
-              </div>
-
-              <div className='flex flex-row justify-between'>
-                {/* Course last modified */}
-                <div>
-                  <LastEdited course={course} />
-                </div>
-
-                {/* Course status */}
-                <div className='flex flex-row'>
-                  <div className={'w-3 h-3 mx-2 rounded-full m-auto '+(statuses[course.status].color ?? statuses.default.color)} />
-                  <p className='italic'>
-                    {statuses[course.status].br ?? statuses.default.br}
-                  </p>
-                </div>
-              </div>
+             
             </div>
           </div>
         </div>
--- app/src/pages/Courses.tsx
+++ app/src/pages/Courses.tsx
@@ -32,6 +32,10 @@ import { useState } from "react";
  * as well as some personal insights about the user.
  */
 const Courses = () => {
+  const [isGridView, setIsGridView] = useState(true); // Default to Grid View
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredCourses, setFilteredCourses] = useState([]);
+  const [selectedFilter, setSelectedFilter] = useState("newest");
   // States and Hooks
   const navigate = useNavigate();
   const token = getUserToken();
@@ -58,6 +62,40 @@ const Courses = () => {
     CourseServices.getAllCourses
   );
 
+  // Filter courses based on the search term
+  useEffect(() => {
+    filterAndSortCourses(data, searchTerm, selectedFilter, setFilteredCourses);
+  }, [searchTerm, data, selectedFilter]);
+
+  function filterAndSortCourses(data: any, searchTerm: string, selectedFilter: string, setFilteredCourses) {
+    if (data && Array.isArray(data)) {
+      const lowerCaseSearchTerm = searchTerm.toLowerCase();
+      let filtered = data.filter(
+        (item: any) => item.title.toLowerCase().includes(lowerCaseSearchTerm) ||
+          item.category.toLowerCase().includes(lowerCaseSearchTerm)
+      );
+  
+      // Apply sorting based on selected filter
+      switch (selectedFilter) {
+        case "newest":
+          filtered.sort((a: any, b: any) => new Date(b.dateCreated) - new Date(a.dateCreated));
+          break;
+        case "oldest":
+          filtered.sort((a: any, b: any) => new Date(a.dateCreated) - new Date(b.dateCreated));
+          break;
+        case "popular":
+          filtered.sort((a: any, b: any) => b.numOfSubscriptions - a.numOfSubscriptions);
+          break;
+        default:
+          break; // 'all' case doesn't need additional sorting
+      }
+  
+      setFilteredCourses(filtered);
+    } else {
+      setFilteredCourses([]);
+    }
+  }
+
   if (error && error.response.status === 401) {
     localStorage.removeItem("token");
     localStorage.removeItem("userInfo");
@@ -72,6 +110,11 @@ const Courses = () => {
       </Layout>
     );
 
+
+    const handleGridOrList = (view) => {
+      setIsGridView(view === 'grid');
+    };
+
   return (
     <Layout meta="Course overview">
       <div className="grid lg:grid-cols-[3fr_1fr] h-full font-personalInsights">
@@ -80,7 +123,7 @@ const Courses = () => {
           {data.length ? (
             <>
               {/* Header and create course button */}
-              <div className="flex flex-row no-wrap">
+              <div className="flex flex-row no-wrap items-center">
                 <h1 className="text-3xl font-bold flex-1">
                   Confira seus cursos
                 </h1>
@@ -88,25 +131,77 @@ const Courses = () => {
                   <label
                     htmlFor="course-create"
                     onClick={CourseManager}
-                    className="std-button flex modal-button space-x-2 cursor-pointer"
+                    className="std-button flex modal-button space-x-2 cursor-pointer px-12 py-2.5"
                   >
-                    <PencilSquareIcon className="w-8 h-8" />
-                    <p className="font-normal ">Criar novo curso</p>{" "}
+                    <p className="font-normal flex items-center "><span className="text-3xl mr-2">+</span> Novo Curso</p>{" "}
                     {/** Create new course */}
                   </label>
                   {/* Course guide button shows a tutorial*/}
-                  <CourseGuideButton />
+                  {/* <CourseGuideButton /> */}
                 </div>
               </div>
               {/* Card/compact view toggle and filters */}
-              <div className="h-10 my-8 bg-grayLight">
+              <div className="my-8 flex justify-between">
                 {/* TODO: Implement card/compact view toggle */}
+                <div className="flex ">
 
+                <button  onClick={() => handleGridOrList('grid')}>
+                  <div id="GridView" className={`p-2 rounded-l-lg border border-grayLight ${isGridView ? "bg-primary": ''}`}>
+                      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8 12C9.1 12 10 12.9 10 14C10 15.1 9.1 16 8 16C6.9 16 6 15.1 6 14C6 12.9 6.9 12 8 12ZM8 6C9.1 6 10 6.9 10 8C10 9.1 9.1 10 8 10C6.9 10 6 9.1 6 8C6 6.9 6.9 6 8 6ZM8 0C9.1 0 10 0.9 10 2C10 3.1 9.1 4 8 4C6.9 4 6 3.1 6 2C6 0.9 6.9 0 8 0ZM2 12C3.1 12 4 12.9 4 14C4 15.1 3.1 16 2 16C0.9 16 0 15.1 0 14C0 12.9 0.9 12 2 12ZM2 6C3.1 6 4 6.9 4 8C4 9.1 3.1 10 2 10C0.9 10 0 9.1 0 8C0 6.9 0.9 6 2 6ZM2 0C3.1 0 4 0.9 4 2C4 3.1 3.1 4 2 4C0.9 4 0 3.1 0 2C0 0.9 0.9 0 2 0ZM14 12C15.1 12 16 12.9 16 14C16 15.1 15.1 16 14 16C12.9 16 12 15.1 12 14C12 12.9 12.9 12 14 12ZM14 6C15.1 6 16 6.9 16 8C16 9.1 15.1 10 14 10C12.9 10 12 9.1 12 8C12 6.9 12.9 6 14 6ZM14 0C15.1 0 16 0.9 16 2C16 3.1 15.1 4 14 4C12.9 4 12 3.1 12 2C12 0.9 12.9 0 14 0Z" fill={isGridView ? "#FFF" : "#A1ACB2"}/>
+                      </svg>
+                  </div>
+                </button>
+
+                  <button  onClick={() => handleGridOrList('list')}>
+                    <div id="ListView" className={`p-2 rounded-r-lg border border-grayLight ${!isGridView ? "bg-primary" : ''}`}>
+                        <svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M0 0H4V4H0V0ZM6 1V3H18V1H6ZM0 6H4V10H0V6ZM6 7V9H18V7H6ZM0 12H4V16H0V12ZM6 13V15H18V13H6Z" fill={!isGridView ? "#FFF" : "#A1ACB2"}/>
+                        </svg>
+                    </div>
+                  </button>
+
+                </div>
+                
+                  <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-full md:space-x-4 space-y-3 md:space-y-0 justify-end py-6">
+                  <div className="relative min-w-[225px] flex-grow-0">
+                    <input
+                      className="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pr-3 shadow-sm focus:outline-none hover:bg-white focus:border-sky-500 focus:ring-1 sm:text-sm"
+                      type="text"
+                      id="search-term"
+                      placeholder="Buscar curso"
+                      onChange={(event) => setSearchTerm(event.target.value)}
+                    />
+                    <svg
+                      className="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-slate-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M21 21l-4.35-4.35m1.35-5.65a7 7 0 11-14 0 7 7 0 0114 0z"
+                      />
+                    </svg>
+                  </div>
+                  <select
+                    onChange={(event) => setSelectedFilter(event.target.value)}
+                    className="block bg-white min-w-[175px] flex-grow-0 border border-slate-300 rounded-md py-2 pr-3 shadow-sm focus:outline-none hover:bg-white focus:border-sky-500 focus:ring-1 sm:text-sm">
+                    <option value="newest">Últimos incluídos</option>
+                    <option value="oldest">Mais antigos</option>
+                    <option value="popular">Mais populares</option>
+
+                  </select>
+                </form>
+                
                 {/* TODO: Implement filters */}
               </div>
               <div className="overflow-y-scroll no-scrollbar">
                 <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4 gap-6 pb-4">
-                  {data.map((course: any, key: number) => (
+                {filteredCourses.map((course: any, key: number) => (
                     <CourseListCard course={course} key={key} />
                   ))}
                 </div>
